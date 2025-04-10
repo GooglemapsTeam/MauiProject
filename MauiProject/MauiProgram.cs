a1473ff19421a723864333ca1449211215bd4ba4
Striker920 <120324168+Striker920@users.noreply.github.com>
2025-04-10 20:29:56 +0500

    Добавьте файлы проекта.

---
+++ MauiProject/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿namespace MenuFormExample;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnOpenMenuButtonClicked(object sender, EventArgs e)
+    {
+        // Создаем экземпляр страницы меню.
+        var menuPage = new MenuPage();
+
+        // Подписываемся на событие MenuItemSelected, чтобы получить результат.
+        menuPage.MenuItemSelected += (s, args) =>
+        {
+            // Устанавливаем текст в метке на основной странице.
+            ResultLabel.Text = $"Вы выбрали: {args.SelectedItem}";
+
+            // Закрываем модальное окно меню.
+            Navigation.PopModalAsync();
+        };
+
+        // Отображаем страницу меню как модальное окно.
+        await Navigation.PushModalAsync(menuPage);
+    }
+}
---
+++ MauiProject/MenuPage.cs
@@ -0,0 +1,36 @@
+﻿//namespace MenuFormExample;
+
+//public partial class MenuPage : ContentPage
+//{
+//    // Объявляем событие, которое будет возникать при выборе пункта меню.
+//    public event EventHandler<MenuItemEventArgs> MenuItemSelected;
+
+//    public MenuPage()
+//    {
+//        InitializeComponent();
+//    }
+
+//    private void OnOption1Clicked(object sender, EventArgs e)
+//    {
+//        // Вызываем событие MenuItemSelected с выбранным значением.
+//        MenuItemSelected?.Invoke(this, new MenuItemEventArgs { SelectedItem = "Option 1" });
+//    }
+
+//    private void OnOption2Clicked(object sender, EventArgs e)
+//    {
+//        // Вызываем событие MenuItemSelected с выбранным значением.
+//        MenuItemSelected?.Invoke(this, new MenuItemEventArgs { SelectedItem = "Option 2" });
+//    }
+
+//    private void OnOption3Clicked(object sender, EventArgs e)
+//    {
+//        // Вызываем событие MenuItemSelected с выбранным значением.
+//        MenuItemSelected?.Invoke(this, new MenuItemEventArgs { SelectedItem = "Option 3" });
+//    }
+//}
+
+//// Класс для передачи данных о выбранном пункте меню.
+//public class MenuItemEventArgs : EventArgs
+//{
+//    public string SelectedItem { get; set; }
+//}
