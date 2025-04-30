1be5b3bb39be77fb8d5ed5aa3c3146d65aef41c8
medyantsevsvyt@gmail.com <medyantsevsvyt@gmail.com>
2025-04-30 13:47:21 +0500

    Добавление второй страницы

---
+++ MauiProject/SecondSurveyPage.xaml.cs
@@ -0,0 +1,15 @@
+namespace Emotional_Map;
+
+public partial class SecondSurveyPage : ContentPage
+{
+	public SecondSurveyPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void OnNextClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new FirstSurveyPage());
+    }
+
+}
